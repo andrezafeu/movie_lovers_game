7e73d361389aef68e814d008ba95a08d03043bfa
Andreza Feu <andrezafeu@gmail.com>
2016-04-08 12:17:30 -0400

    Display the title of the first 9 movies that match the search_term

--- lib/movies.rb
+++ lib/movies.rb
@@ -1,3 +1,27 @@
 class Movies
-
+	attr_reader :movies
+	def initialize
+		@movies = []
+	end
+	def search(params)
+		@params = params
+		search = Imdb::Search.new(params)
+		i=0
+		while @movies.length<9
+			@movies.push(search.movies[i])
+			i = i + 1
+		end
+		@movies
+	end
 end
+
+
+
+# FARAZ:
+# class Movies
+# 	def search(params)
+# 		search = Imdb::Search.new(params)
+# 		movies = search.movies
+# 		movies
+# 	end
+# end
--- server.rb
+++ server.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'imdb'
+require 'pry'
 
 require_relative('lib/movies.rb')
+
+new_search = Movies.new
 
 get "/home" do
 	erb :home_page
 end 
+
+post "/new_search" do
+	@search_array = new_search.search(params[:search_term])
+	erb :new_search
+end
